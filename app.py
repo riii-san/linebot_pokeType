f06f97d681a433eb0d68a29f2eb8bc4904a92497
中野稜也 <ryoya@nakanoryounarinoMacBook-Pro.local>
2022-01-16 21:10:38 +0900

    複数のpythonファイルを保存

--- app.py
+++ app.py
@@ -45,9 +45,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # line_bot_api.reply_message(
-    #     event.reply_token,
-    #     TextSendMessage(text=event.message.text))
+
     sendMessage = calPT.calPokeTypeMain(event.message.text)
 
     if sendMessage != False :
